bc4d813393fc19068a9d45d41e7ab2913095706d
Nathan <nathan.istace@gmail.com>
2020-05-17 10:36:41 +0200

    Editor for utils as an independent repository

---
+++ CustomEditors/LibraryCustomEditor.cs
@@ -0,0 +1,72 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace UtilsEditor {
+	public abstract class LibraryCustomEditor<E> : Editor {
+		public override void OnInspectorGUI() {
+			var library = (Library<E>) target;
+			var itemsCount = Mathf.Min(serializedObject.FindProperty("_itemNames").arraySize, serializedObject.FindProperty("_items").arraySize);
+			GUI.enabled = false;
+			EditorGUILayout.ObjectField("Script:", MonoScript.FromScriptableObject(library), typeof(Library<E>), false);
+			GUI.enabled = true;
+
+			EditorGUILayout.PropertyField(serializedObject.FindProperty("_id"));
+			EditorGUILayout.PropertyField(serializedObject.FindProperty("_defaultItem"));
+			EditorGUILayout.PropertyField(serializedObject.FindProperty("_orderIndex"));
+			AddOtherFields();
+			GUILayout.Label("Items:");
+			for (var i = 0; i < itemsCount; ++i) {
+				GUILayout.BeginHorizontal();
+				EditorGUILayout.PropertyField(serializedObject.FindProperty("_itemNames").GetArrayElementAtIndex(i), GUIContent.none);
+				EditorGUILayout.PropertyField(serializedObject.FindProperty("_items").GetArrayElementAtIndex(i), GUIContent.none);
+				if (GUILayout.Button("Copy", GUILayout.Width(50))) {
+					serializedObject.FindProperty("_itemNames").InsertArrayElementAtIndex(i + 1);
+					serializedObject.FindProperty("_itemNames").GetArrayElementAtIndex(i + 1).stringValue = serializedObject.FindProperty("_itemNames").GetArrayElementAtIndex(i).stringValue + ".copy";
+					serializedObject.FindProperty("_items").InsertArrayElementAtIndex(i + 1);
+					CopyValue(serializedObject.FindProperty("_items").GetArrayElementAtIndex(i), serializedObject.FindProperty("_items").GetArrayElementAtIndex(i + 1));
+				}
+				if (GUILayout.Button("-", GUILayout.Width(30))) {
+					serializedObject.FindProperty("_itemNames").DeleteArrayElementAtIndex(i);
+					serializedObject.FindProperty("_items").DeleteArrayElementAtIndex(i);
+					itemsCount--;
+					i--;
+				}
+
+				GUILayout.EndHorizontal();
+			}
+
+			if (GUILayout.Button("+")) {
+				if (serializedObject.FindProperty("_itemNames").arraySize == itemsCount)
+					serializedObject.FindProperty("_itemNames").InsertArrayElementAtIndex(itemsCount);
+				if (serializedObject.FindProperty("_items").arraySize == itemsCount)
+					serializedObject.FindProperty("_items").InsertArrayElementAtIndex(itemsCount);
+			}
+			if (GUILayout.Button("Sort alphabetically")) {
+				library.SortItems();
+			}
+			if (GUILayout.Button("Reload")) {
+				library.Load();
+			}
+			serializedObject.ApplyModifiedProperties();
+		}
+
+		protected abstract void CopyValue(SerializedProperty origin, SerializedProperty to);
+
+		public virtual void AddOtherFields() { }
+	}
+
+	[CustomEditor(typeof(ColorLibrary))]
+	public class ColorLibraryCustomEditor : LibraryCustomEditor<Color> {
+		protected override void CopyValue(SerializedProperty origin, SerializedProperty to) => to.colorValue = origin.colorValue;
+	}
+
+	[CustomEditor(typeof(SpriteLibrary))]
+	public class SpriteLibraryCustomEditor : LibraryCustomEditor<Sprite> {
+		protected override void CopyValue(SerializedProperty origin, SerializedProperty to) => to.objectReferenceValue = origin.objectReferenceValue;
+	}
+
+	[CustomEditor(typeof(ConstantLibrary))]
+	public class ConstantLibraryCustomEditor : LibraryCustomEditor<string> {
+		protected override void CopyValue(SerializedProperty origin, SerializedProperty to) => to.stringValue = origin.stringValue;
+	}
+}
---
+++ Properties/DataIdCustomPropertyDrawer.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+namespace MSGEditor {
+	[CustomPropertyDrawer(typeof(DataId))]
+	public class DataIdCustomPropertyDrawer : PropertyDrawer {
+		public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
+			EditorGUI.BeginProperty(position, label, property);
+
+			position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
+
+			var indent = EditorGUI.indentLevel;
+			EditorGUI.indentLevel = 0;
+
+			var w6 = position.width / 6;
+
+			EditorGUI.PropertyField(new Rect(position.x, position.y, w6, position.height), property.FindPropertyRelative("_value"), GUIContent.none);
+
+			if (property.FindPropertyRelative("_value").intValue == 0 || GUI.Button(new Rect(position.x + w6, position.y, 4 * w6, position.height), "Next")) {
+				property.FindPropertyRelative("_value").intValue = DataId.GetNextId();
+			}
+
+			if (property.FindPropertyRelative("_value").intValue == 0 || GUI.Button(new Rect(position.x + 5 * w6, position.y, w6, position.height), "Check")) {
+				var identities = Resources.LoadAll<DataMonoBehaviour>("").Cast<IData>().Union(Resources.LoadAll<DataScriptableObject>("")).ToList();
+				identities.Sort((t, u) => t.id - u.id);
+				var countErrors = 0;
+				for (var i = 0; i < identities.Count - 1; ++i) {
+					if (identities[i].id != identities[i + 1].id) continue;
+					countErrors++;
+					Debug.LogError("Two objects with the same id: " + identities[i] + " and " + identities[i + 1] + ". ID " + identities[i].id);
+				}
+				if (countErrors == 0) Debug.Log("No ID error found.");
+				else Debug.LogWarning(countErrors + " errors found.");
+			}
+
+			EditorGUI.indentLevel = indent;
+			EditorGUI.EndProperty();
+		}
+	}
+}
