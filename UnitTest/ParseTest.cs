6863897a0f19ae35727dcb6bcf1315824b0eaa0a
Nathan <nathan.istace@gmail.com>
2021-01-19 21:21:09 +0100

    Parsing to float unit tests

---
+++ UnitTest/ParseTest.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using Utils.StaticUtils;
+
+namespace UtilsEditor.Tests {
+	public static class ParseTest {
+		[Test]
+		public static void TestTryParseFloat() {
+			Assert.AreEqual(0, 0);
+			Assert.IsTrue(Parse.TryFloat("0", out var t));
+			Assert.AreEqual(0, t);
+			Assert.IsTrue(Parse.TryFloat("1", out t));
+			Assert.AreEqual(1, t);
+			Assert.IsTrue(Parse.TryFloat("1.0", out t));
+			Assert.AreEqual(1, t);
+			Assert.IsTrue(Parse.TryFloat("1,0", out t));
+			Assert.AreEqual(1, t);
+			Assert.IsTrue(Parse.TryFloat($"{1f:0}", out t));
+			Assert.AreEqual(1, t);
+			Assert.IsTrue(Parse.TryFloat($"{1f:0.0}", out t));
+			Assert.AreEqual(1, t);
+			Assert.IsTrue(Parse.TryFloat("1.5", out t));
+			Assert.AreEqual(1.5f, t);
+			Assert.IsTrue(Parse.TryFloat("1,5", out t));
+			Assert.AreEqual(1.5f, t);
+			Assert.IsTrue(Parse.TryFloat($"{1.4f:0}", out t));
+			Assert.AreEqual(1, t);
+			Assert.IsTrue(Parse.TryFloat($"{1.5f:0.0}", out t));
+			Assert.AreEqual(1.5f, t);
+		}
+	}
+}
