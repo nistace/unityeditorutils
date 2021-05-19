20acdcff905a8bb5a3040e4ee0208acfd3bae7a0
Nathan <nathan.istace@gmail.com>
2021-05-19 19:09:08 +0200

    DataId: Moved the check function to the Tools bar. Created a function to get the max id valuer

--- Properties/DataIdCustomPropertyDrawer.cs
+++ Properties/DataIdCustomPropertyDrawer.cs
@@ -1,5 +1,4 @@
-﻿using System.Linq;
-using UnityEditor;
+﻿using UnityEditor;
 using UnityEngine;
 using Utils.Id;
 
@@ -14,25 +13,10 @@ namespace UtilsEditor.Properties {
 			var indent = EditorGUI.indentLevel;
 			EditorGUI.indentLevel = 0;
 
-			var w6 = position.width / 6;
+			EditorGUI.PropertyField(new Rect(position.x, position.y, 80, position.height), property.FindPropertyRelative("_value"), GUIContent.none);
 
-			EditorGUI.PropertyField(new Rect(position.x, position.y, w6, position.height), property.FindPropertyRelative("_value"), GUIContent.none);
-
-			if (property.FindPropertyRelative("_value").intValue == 0 || GUI.Button(new Rect(position.x + w6, position.y, 4 * w6, position.height), "Next")) {
+			if (property.FindPropertyRelative("_value").intValue == 0 || GUI.Button(new Rect(position.x + 80, position.y, position.width - 80, position.height), "Next")) {
 				property.FindPropertyRelative("_value").intValue = DataId.GetNextId();
-			}
-
-			if (property.FindPropertyRelative("_value").intValue == 0 || GUI.Button(new Rect(position.x + 5 * w6, position.y, w6, position.height), "Check")) {
-				var identities = Resources.LoadAll<DataMonoBehaviour>("").Cast<IData>().Union(Resources.LoadAll<DataScriptableObject>("")).ToList();
-				identities.Sort((t, u) => t.id - u.id);
-				var countErrors = 0;
-				for (var i = 0; i < identities.Count - 1; ++i) {
-					if (identities[i].id != identities[i + 1].id) continue;
-					countErrors++;
-					Debug.LogError("Two objects with the same id: " + identities[i] + " and " + identities[i + 1] + ". ID " + identities[i].id);
-				}
-				if (countErrors == 0) Debug.Log("No ID error found.");
-				else Debug.LogWarning(countErrors + " errors found.");
 			}
 
 			EditorGUI.indentLevel = indent;
